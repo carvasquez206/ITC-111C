de6207db6fce53f3a7a952a792d36ec27a73baf7
carvasquez206 <63617482+carvasquez206@users.noreply.github.com>
2020-06-11 13:11:58 -0700

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,13 +2,13 @@ var val1, val2;
 function loadDisplay(num){
   document.getElementById('display2').value += num.value.toString();
 }
-function bckspDisplay(){
+function delFromRight(){
   document.getElementById('display2').value = document.getElementById('display2').value.slice(0,-1);
 }
 function clearDisplay(){
   document.getElementById('display2').value = "";
 }
-function setOp(trg){
+function setOperator(tag){
   val1 = document.getElementById('display2').value + " " + tag.value;
   document.getElementById('display1').innerHTML = val1;
   document.getElementById('display2').value = "";
